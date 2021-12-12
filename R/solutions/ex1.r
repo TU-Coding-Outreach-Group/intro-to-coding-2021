a31b39ea45f4ed32cc350617ee553899473a8e89
Haroon Popal <popal.haroon@gmail.com>
2021-12-12 15:32:18 -0500

    pre-workshop formatting

---
+++ R/solutions/ex1.r
@@ -0,0 +1,17 @@
+  install.packages(c("effects", "ggpubr", "ggthemes", "interactions", "psych", "plyr", "rstatix", 
+  "RColorBrewer", "report", "sjPlot", "sjstats", "skimr", "stargazer", "tidyverse", "pwr"))
+  
+  library(effects)  
+  library(ggpubr)  
+  library(ggthemes)  
+  library(interactions)  
+  library(psych)  
+  library(plyr)  
+  library(rstatix)
+  library(RColorBrewer) 
+  library(report)
+  library(sjPlot)
+  library(sjstats)
+  library(skimr) 
+  library(stargazer)
+  library(tidyverse)
