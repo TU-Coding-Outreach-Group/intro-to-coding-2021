7ce69d4ee5dcf9ae887710f94df11c52d60b28b6
Haroon Popal <popal.haroon@gmail.com>
2022-08-09 18:04:26 -0400

    fixed syntax error

--- bash/solutions/ex3.sh
+++ bash/solutions/ex3.sh
@@ -1,6 +1,6 @@
 # Use a for loop to check whether data exists for all subjects
 for subj in DE201 DE202 DE203 DE204 DE205 DE206 DE207 DE208 DE209 DE210; do
-    if (! -e ${subj}/${subj}_dbdm_run_1.csv); then
+    if [ ! -e ${subj}/${subj}_dbdm_run_1.csv ]; then
         echo ${subj}
     fi
 done
